b1c0b2148d299b5c175a800f856c410ceff1a6be
nate <nwvbug@gmail.com>
2025-01-20 17:45:26 -0800

    thing

--- nwvbug.py
+++ nwvbug.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html") 
+    return redirect("https://github.com/nwvbug")
 
 
     
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from nwvbug import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run()   
