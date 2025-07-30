060de603c652e8553f31a4f28451f24148fe5541
nwvbug <nwvbug@gmail.com>
2025-07-30 16:58:42 -0700

    flask

--- nwvbug.py
+++ nwvbug.py
@@ -1,14 +1,21 @@
+import os
 from flask import Flask
-from flask import render_template, redirect
+from flask import render_template, redirect, send_file, abort
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return redirect("https://github.com/nwvbug")
+    return render_template("index.html")
 
 
-    
+@app.route("/assets/<image>")
+def assets(image):
+    try:
+        file_path = os.path.join('static', image)
+        return send_file(file_path)
+    except FileNotFoundError:
+        return abort(404)
 
 print("Server starting: NWVBUG Homepage server")
 if __name__ == "__main__":
